766e49eb678defce655c5d2e2933717557b8d0fe
elahe-habibi98 <115075942+elahe-habibi98@users.noreply.github.com>
2023-03-28 18:39:34 +0430

    .

--- src/components/hourly/chart.tsx
+++ src/components/hourly/chart.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FC } from "react";
 
 const Chart: FC = (): JSX.Element => {
-  return (<div>Chart</div>);
+  return (<div>Chart2</div>);
 };
 
 export default Chart;
